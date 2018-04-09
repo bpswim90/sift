3a3ccd380e452e4f81728030ef10b3c52db51560
William Paulsen <bpswim90@gmail.com>
2018-04-09 14:07:29 -0500

    Create ResultCard.vue

---
+++ src/components/ResultCard.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="card" v-show="name">
+        <img class="card-img-top" v-bind:src="img">
+        <div class="card-body">
+            <h4 class="card-title">{{name}}</h4>
+            <small v-show="url">by <a v-bind:href="url" class="card-link">{{source}}</a></small>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+
+
+    }
+</script>
+
+<style>
+    .card {display: inline-block}
+</style>
