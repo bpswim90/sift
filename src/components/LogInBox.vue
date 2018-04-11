331a0124c463f462321dfbe6639375ec39b51d56
William Paulsen <bpswim90@gmail.com>
2018-04-11 18:48:28 -0500

    Build LogInBox template

--- src/components/LogInBox.vue
+++ src/components/LogInBox.vue
@@ -1,6 +1,17 @@
 <template>
-    <div>
-        Hello!
+    <div class="card">
+        <div class="card-body p-5">
+            <h5 class="card-title mb-3">Welcome to sift.</h5>
+            <p class="card-subtitle text-muted mb-4">Find your next great meal.</p>
+            <form>
+                <input type="email" class="form-control mb-1" placeholder="E-mail">
+                <input type="password" class="form-control mb-1" placeholder="Password">
+                <button type="submit" class="btn btn-primary form-control">Log In</button>
+            </form>
+        </div>
+        <div class="card-footer">
+            Not a member yet? Sign up!
+        </div>
     </div>
 </template>
 
@@ -11,4 +22,8 @@ module.exports = {
 </script>
 
 <style>
+.card {
+    width: 400px;
+    margin: auto;
+}
 </style>
