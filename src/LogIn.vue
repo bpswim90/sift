d72b0d7806c2752981c326abeaef713b683e13ba
William Paulsen <bpswim90@gmail.com>
2018-04-14 20:26:03 -0500

    Refactor LogIn into one component

--- src/LogIn.vue
+++ src/LogIn.vue
@@ -1,17 +1,25 @@
 <template>
 <div id="app">
-    <log-in-box id="login" class="mx-auto"></log-in-box>
+    <div class="card mx-auto" id="login">
+        <div class="card-body p-5">
+            <h5 class="card-title mb-3">Welcome to sift.</h5>
+            <p class="card-subtitle text-muted mb-5">Search for recipes from your favorite sites across the internet.</p>
+            <form>
+                <input type="email" class="form-control mb-1" placeholder="E-mail">
+                <input type="password" class="form-control mb-1" placeholder="Password">
+                <button type="submit" class="btn btn-primary form-control">Log In</button>
+            </form>
+        </div>
+        <div class="card-footer text-muted text-center">
+            <small>Not a member yet? Sign up!</small>
+        </div>
+    </div>
 </div>
 </template>
 
 <script>
-var LogInBox = require("./components/LogInBox.vue")
-
 module.exports = {
-    name: 'app',
-    components: {
-        'log-in-box': LogInBox
-    }
+    name: 'app'
 }
 </script>
 
