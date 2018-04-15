81f731e10948b3a70be546ca6af92bc404e23900
William Paulsen <bpswim90@gmail.com>
2018-04-14 19:04:43 -0500

    Set up navigation guard that redirects to /login if user not logged in

--- src/main.js
+++ src/main.js
@@ -7,13 +7,36 @@ var App = require('./App.vue')
 Vue.use(VueRouter)
 
 var routes = [
-    { path: '', component: LogIn },
-    { path: '/login', component: LogIn},
-    { path: '/search', component: Search}
+    { 
+        path: '', 
+        component: LogIn
+    },
+    { 
+        path: '/login', 
+        component: LogIn
+    },
+    {
+        path: '/search',
+        component: Search,
+        meta: {
+            requiresAuth: true
+        }
+    }
 ]
 
 var router = new VueRouter({
     routes: routes
+})
+
+//Global navigation guard: redirects to /login if page requires login
+router.beforeEach((to, from, next) => {
+    var requiresAuth = to.matched.some(record => record.meta.requiresAuth)
+
+    if (requiresAuth) {
+        next('/login');
+    } else {
+        next();
+    }
 })
 
 new Vue({ // eslint-disable-line no-new
