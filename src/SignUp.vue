f4db15b328dd2812f399ca7190e1079e2623d8a5
William Paulsen <bpswim90@gmail.com>
2018-04-14 20:36:05 -0500

    Create SignUp page

---
+++ src/SignUp.vue
@@ -0,0 +1,31 @@
+<template>
+<div id="app">
+    <div class="card mx-auto" id="signup">
+        <div class="card-body p-5">
+            <h5 class="card-title mb-3">Sign Up</h5>
+            <p class="card-subtitle text-muted mb-5">Search for recipes from your favorite sites across the internet.</p>
+            <form>
+                <input type="email" class="form-control mb-1" placeholder="E-mail">
+                <input type="password" class="form-control mb-1" placeholder="Password">
+                <button type="submit" class="btn btn-primary form-control">Sign Up</button>
+            </form>
+        </div>
+        <div class="card-footer text-muted text-center">
+            <small>Already signed up? Go here to log in.</small>
+        </div>
+    </div>
+</div>
+</template>
+
+<script>
+module.exports = {
+    name: 'app'
+}
+</script>
+
+<style>
+#signup {
+    display: block;
+    width: 400px
+}
+</style>
