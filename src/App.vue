158462d20634e84b2acfdd72be4b2bbb54151d37
William Paulsen <bpswim90@gmail.com>
2018-04-16 13:29:36 -0500

    Remove test router links

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,5 @@
 <template>
 <div>
-    <router-link to="/search">Search</router-link>
-    <router-link to="/login">LogIn</router-link>
-    <router-link to="/signup">SignUp</router-link>
-
     <router-view></router-view>
 </div>
 </template>
