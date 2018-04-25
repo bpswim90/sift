5efca18d9086fe1b93a0220bb349b7e7532208e5
William Paulsen <bpswim90@gmail.com>
2018-04-24 22:11:06 -0500

    Result card saves to database

--- src/components/ResultCard.vue
+++ src/components/ResultCard.vue
@@ -5,12 +5,34 @@
             <h4 class="card-title">{{name}}</h4>
             <small v-show="url">by <a v-bind:href="url" class="card-link">{{source}}</a></small>
         </div>
+        <div class="card-footer">
+            <button type="button" class="btn btn-outline-danger" data-toggle="button"
+                v-on:click="addToUserFavorites()">Add Site to Favorites</button>
+        </div>
     </div>
 </template>
 
 <script>
+var Firebase = require('firebase')
+
 module.exports = {
-    props: ['name','img','url','source']
+    props: ['name','img','url','source'],
+    computed: {
+        userId: function() {
+            var user = Firebase.auth().currentUser
+
+            if (user !== null) {
+                return user.uid
+            }
+        }
+    },
+    methods: {
+       addToUserFavorites: function() {
+           var app = this
+
+           Firebase.database().ref('users/' + this.userId).child('favorites').push(this.source)
+       }
+    }
 }
 </script>
 
