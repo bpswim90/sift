9c5bff7328db094fb9d882a59e094fc60376e520
William Paulsen <bpswim90@gmail.com>
2018-05-11 14:43:01 -0500

    Create Collection template

---
+++ src/Collection.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h1>
+            {{collectionId}}
+        </h1>
+    </div>
+</template>
+
+<script>
+module.exports = {
+    props: ['collectionId']
+}
+</script>
+
+<style>
+</style>
